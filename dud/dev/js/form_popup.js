2a38e59f8418780d02bdb5392784f126c44ed910
Zain MATHEWS <zmathews@c5r11s5.wethinkcode.co.za>
2019-11-17 14:46:50 +0200

    Basically went backwards with single page, no php linking

--- dud/dev/js/form_popup.js
+++ dud/dev/js/form_popup.js
@@ -1,8 +1,8 @@
 function if_empty() {
-	if (document.getElementById('').value == "" ||document.getElementById('') == "") {
+	if (document.getElementById('login-email').value == "" || document.getElementById('login-password') == "") {
 		alert('Fill all fields');
 	}
-	else {
+	else if (document.getElementById('login-email').value != "" && document.getElementById('login-password').value != "") {
 		document.getElementById('login-form').submit();
 		alert('Login Successful');
 	}
@@ -14,4 +14,21 @@ function show_log() {
 
 function hide_log() {
 	document.getElementById('pop').style.display = "none";
+}
+
+function reg_empty() {
+	if (document.getElementById('reg-name').value == "" || document.getElementById('reg-lname').value == "" || document.getElementById('reg-uname').value == "" || document.getElementById('reg-email').value == "" || document.getElementById('reg-p1').value == "" || document.getElementById('reg-p2').value == "") {
+		alert('Fill all fields');
+	}
+	else if (document.getElementById('reg-name').value != "" && document.getElementById('reg-lname').value != "" && document.getElementById('reg-uname').value != "" && document.getElementById('reg-email').value != "" && document.getElementById('reg-p1').value != "" && document.getElementById('reg-p2').value != "") {
+		alert('Register Successful');
+	}
+}
+
+function show_reg() {
+	document.getElementById('pop2').style.display = "block";
+}
+
+function hide_reg() {
+	document.getElementById('pop2').style.display = "none";
 }
--- dud/index.php
+++ dud/index.php
@@ -1,3 +1,7 @@
+<?php
+	
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,16 +13,56 @@
 	<title>Camagru Home</title>
 </head>
 <body style="overflow:hidden;">
-  <h1>Welcome to Camagru</h1>
-  <div id="pop">
+	<h1>Welcome to Camagru</h1>
+	<div id="pop2">
+		<div id="popupRegister">
+			<form action="#" id="register-form" method="post" name="register-form">
+				<h2>Create an account</h2>
+
+				<img id="close" src="./user/resources/close_button.png" onclick="hide_reg()">
+
+				<hr>
+
+				<input id="reg-name" name="reg-name" placeholder="Name" type="text" required>
+
+				<br>
+
+				<input id="reg-lname" name="reg-lname" placeholder="Surname" type="text" required>
+
+				<br>
+
+				<input id="reg-uname" name="reg-uname" placeholder="Password" type="password" required>
+
+				<br>
+
+				<input id="reg-email" name="reg-email" placeholder="Email" type="email" required>
+
+				<br>
+
+				<input id="reg-p1" name="reg-p1" placeholder="Password" type="password" required>
+
+				<br>
+
+				<input id="reg-p2" name="reg-p2" placeholder="Confirm Password" type="password" required>
+
+				<a href="javascript:%20if_empty()" id="submit">Register</a>
+			</form>
+		</div>
+	</div>
+	<div id="pop">
 		<div id="popupLogin">
 			<form action="#" id="login-form" method="post" name="login-form">
-				<img id="close" src="./user/resources/camera_logo.jpg" onclick="hide_log()">
+				<img id="close" src="./user/resources/close_button.png" onclick="hide_log()">
 
-				<input id="login-email" name="email" placeholder="Email" type="email">
-				<input id="login-password" name="password" placeholder="Password" type="password">
+				<hr>
 
-				<a href="javascript:%20check_empty()" id="submit">Send</a>
+				<input id="login-email" name="email" placeholder="Email" type="email" required>
+
+				<br>
+
+				<input id="login-password" name="password" placeholder="Password" type="password" required>
+
+				<a href="javascript:%20if_empty()" id="submit">Login</a>
 			</form>
 		</div>
 	</div>
